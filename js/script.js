19d94eca602d022b24ef2451046193d23c3c1acd
Turcan Ilya <ilieturcan424@gmail.com>
2024-06-28 00:40:52 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
 window.addEventListener("DOMContentLoaded", () => {
-    function req() {
-        getResources("http://localhost:3000/people")
+    const form = document.querySelector("form");
+    
+    function req(e) {
+
+        e.preventDefault(); // Чтобы форма не перезагружала страницу при нажатии кнопки.
+
+        let formData = new FormData(form);
+        formData.append("id", Math.random()); // просто чтобы показать, что мы можем аппендить в объект formdata пару ключ значение.
+        // Дальше мы не можем просто сделать  let json = JSON.stringify(formData). Это работать не будет. Если мы хоти отправить объект formdata в json файл, нужно сделать то что ниже. Просто перезаписать все в обычный js объект, а затем js объект переделать в json формат.
+
+        let obj = {};
+        
+        formData.forEach((value, key) => {
+            obj[key] = value;
+        });
+
+
+        getResources("http://localhost:3000/people", obj)
             .then(data => createCards(data))
             .catch(err => console.error(err));
         
-        this.remove();
     }
 
-    document.querySelector("button").addEventListener("click", req, { "once": true });
+    form.addEventListener("submit", (e) => req(e), {"once": true});
     
-    async function getResources(url) {
-        const res = await fetch(`${url}`);
+    async function getResources(url, data) {
+        const res = await fetch(`${url}`, {
+            method: "POST", // по умолчанию get
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
 
         if (!res.ok) { // Своство ok говорит - у нас все прошло успешно и запрос выполнился
             throw new Error(`Could not fetch ${url}, status: ${res.status}`); // Своство status такоже как и в XMLHttpRequest
